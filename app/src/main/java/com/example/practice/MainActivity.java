c05a840fd75774c22a1a4e70e85adca8fa3c493f
Алексей Оспельников <ospelnikov00@mail.ru>
2024-02-13 07:25:37 +0300

    Manifest fixed

--- app/src/main/java/com/example/practice/MainActivity.java
+++ app/src/main/java/com/example/practice/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.practice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private ImageButton addNewDocumentButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        addNewDocumentButton = findViewById(R.id.addButton);
+        addNewDocumentButton.setOnClickListener(this);
+        // TODO: Здесь будет выгрузка всех текущих документов из бд
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        // TODO: Здесь надо будет сделать добавление нового документа в базу данных
+        Intent openTextEditorActivity = new Intent(this, TextEditor.class);
+        startActivity(openTextEditorActivity);
     }
 }
