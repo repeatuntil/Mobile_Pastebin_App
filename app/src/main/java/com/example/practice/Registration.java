91804fe21a7d3ed6b64fa1ddf2d1297b0042c044
Алексей Оспельников <ospelnikov00@mail.ru>
2024-02-13 07:09:42 +0300

    main java files

---
+++ app/src/main/java/com/example/practice/Registration.java
@@ -0,0 +1,12 @@
+package com.example.practice;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Registration extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register);
+    }
+}
---
+++ app/src/main/java/com/example/practice/TextEditor.java
@@ -0,0 +1,12 @@
+package com.example.practice;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TextEditor extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.text_editor);
+    }
+}
