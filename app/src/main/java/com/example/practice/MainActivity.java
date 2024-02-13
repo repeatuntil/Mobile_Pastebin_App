a449be5e38b9f1ecd69c88619dcdd603f819452f
Алексей Оспельников <ospelnikov00@mail.ru>
2024-02-13 07:43:03 +0300

    Добавлены переходы

--- app/src/main/java/com/example/practice/MainActivity.java
+++ app/src/main/java/com/example/practice/MainActivity.java
@@ -9,20 +9,33 @@ import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private ImageButton addNewDocumentButton;
+    private ImageButton toSettingsButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         addNewDocumentButton = findViewById(R.id.addButton);
         addNewDocumentButton.setOnClickListener(this);
+
+        toSettingsButton = findViewById(R.id.settingsButton);
+        toSettingsButton.setOnClickListener(this);
         // TODO: Здесь будет выгрузка всех текущих документов из бд
     }
 
 
     @Override
-    public void onClick(View view) {
+    public void onClick(View clickedButton) {
         // TODO: Здесь надо будет сделать добавление нового документа в базу данных
-        Intent openTextEditorActivity = new Intent(this, TextEditor.class);
-        startActivity(openTextEditorActivity);
+
+        if (clickedButton.getId() == R.id.settingsButton) {
+            Intent openSettingsActivity = new Intent(this, Settings.class);
+            startActivity(openSettingsActivity);
+        }
+        else {
+            Intent openTextEditorActivity = new Intent(this, TextEditor.class);
+            startActivity(openTextEditorActivity);
+        }
     }
+
 }
