71a4f7bf456d65c23805e048e01084ff16d74553
svgarik <svgarik@mail.ru>
2024-02-14 18:02:40 +0300

    feat: удаление файла, создание в хранилище при запуске,

--- app/src/main/java/com/example/practice/TextEditor.java
+++ app/src/main/java/com/example/practice/TextEditor.java
@@ -12,6 +12,8 @@ import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.regions.Region;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.ListObjectsRequest;
+import com.amazonaws.services.s3.model.ObjectListing;
 import com.amazonaws.services.s3.model.S3Object;
 
 import java.io.File;
@@ -41,6 +43,9 @@ public class TextEditor extends Activity {
         if (openStat == OpenTextEditorStatus.OldFile)
         {
             getFile();
+        } else if (openStat == OpenTextEditorStatus.NewFile)
+        {
+            putFile();
         }
     }
 
@@ -48,7 +53,7 @@ public class TextEditor extends Activity {
 
         Context context = getApplicationContext();
 
-        File tempFilePath = new File(context.getFilesDir(), "");
+        File tempFilePath = new File(context.getFilesDir(), "textBuffer.txt");
 
         FileWriter writer = null;
         try {
@@ -63,9 +68,28 @@ public class TextEditor extends Activity {
         return tempFilePath;
     }
 
-    private void putFile() {
-        EditText textEditor = findViewById(R.id.plain_text_input);
-        String text = textEditor.getText().toString();
+    private boolean checkFile(AmazonS3Client s3Client) {
+        //Данные объекта
+        String bucketName = "osgyqgrczgejueayrttvztamhzyawribhyjveaxyifjmgtgckw";
+        String keyName = String.valueOf(_fileId);
+
+        ExecutorService service = Executors.newSingleThreadExecutor();
+        Future<Boolean> future = service.submit(new Callable<Boolean>() {
+            @Override
+            public synchronized Boolean call() throws Exception {
+                return s3Client.doesObjectExist(bucketName, keyName);
+            }
+        });
+
+        try {
+            boolean isExist = future.get();
+            return isExist;
+        } catch (ExecutionException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private AmazonS3Client configClient() {
 
         //Подключение к s3 серверу
         String accessKey = "YCAJEL4iy2T_galhGQdLy4yWj";
@@ -78,6 +102,17 @@ public class TextEditor extends Activity {
         s3Client.setRegion(Region.getRegion(Regions.US_EAST_1));
         s3Client.setEndpoint("storage.yandexcloud.net");
 
+        return s3Client;
+    }
+
+    private void putFile() {
+        EditText textEditor = findViewById(R.id.plain_text_input);
+        String text = textEditor.getText().toString();
+
+        //Подключение к s3 серверу
+        AmazonS3Client s3Client = configClient();
+
+        //Данные объекта
         String bucketName = "osgyqgrczgejueayrttvztamhzyawribhyjveaxyifjmgtgckw";
         String keyName = String.valueOf(_fileId);
 
@@ -107,20 +142,17 @@ public class TextEditor extends Activity {
 
     private void getFile() {
         //Подключение к s3 серверу
-        String accessKey = "YCAJEL4iy2T_galhGQdLy4yWj";
-        String secretKey = "YCPy2-dfJPDUTUz1LLZHCq2mW2cWgaqkPi0qC5mn";
-        BasicAWSCredentials awsCreds = new BasicAWSCredentials (accessKey, secretKey);
+        AmazonS3Client s3Client = configClient();
 
-        ClientConfiguration clientConfig = new ClientConfiguration();
-        clientConfig.setProtocol(Protocol.HTTPS);
-        AmazonS3Client s3Client = new AmazonS3Client(awsCreds, clientConfig);
-        s3Client.setRegion(Region.getRegion(Regions.US_EAST_1));
-        s3Client.setEndpoint("storage.yandexcloud.net");
-
+        //Данные объекта
         String bucketName = "osgyqgrczgejueayrttvztamhzyawribhyjveaxyifjmgtgckw";
         String keyName = String.valueOf(_fileId);
 
-        //Загрузка файла на s3
+        if (!checkFile(s3Client)) {
+            return;
+        }
+
+        //Чтение файла с s3
         ExecutorService service = Executors.newSingleThreadExecutor();
 
         Future<String> future = service.submit(new Callable<String>() {
@@ -135,6 +167,36 @@ public class TextEditor extends Activity {
             // Вставляем текст в редактор
             EditText textEditor = findViewById(R.id.plain_text_input);
             textEditor.setText(text);
+        } catch (ExecutionException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        service.shutdown();
+    }
+
+    private void deleteFile() {
+        //Подключение к s3 серверу
+        AmazonS3Client s3Client = configClient();
+
+        //Данные объекта
+        String bucketName = "osgyqgrczgejueayrttvztamhzyawribhyjveaxyifjmgtgckw";
+        String keyName = String.valueOf(_fileId);
+
+        if (!checkFile(s3Client)) {
+            return;
+        }
+
+        //Загрузка файла на s3
+        ExecutorService service = Executors.newSingleThreadExecutor();
+
+        Future future = service.submit(new Runnable() {
+            public void run() {
+                s3Client.deleteObject(bucketName, keyName);
+            }
+        });
+
+        try {
+            future.get();
         } catch (ExecutionException | InterruptedException e) {
             throw new RuntimeException(e);
         }
